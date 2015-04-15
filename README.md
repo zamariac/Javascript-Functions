5e786812428f5c8ed5e53b4e84089f91e65060e8
Aaron Larner <anlarner@gmail.com>
2015-04-15 14:55:25 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# JavaScript Functions
+
+## Description
+Practice writing some JavaScript functions.
+
+
+## Objectives
+
+### Learning Objectives
+
+After completing this assignment, you should…
+
+* Understand how to write a JavaScript function.
+* Understand the terminology related to defining functions.
+
+
+## Details
+
+### Deliverables
+
+* A repo containing at least:
+  * `functions.js`
+  * `index.html`
+
+### Requirements
+
+* No JSHint warnings or errors
+
+
+## Normal Mode
+Copy and paste the contents of the [functions.js](functions.js) file into your own repo. Read the comments in the file and complete the instructions. Be sure to validate the input for all functions.
+
+
+## Notes
+
+Notes go here...
+
+## Additional Resources
+
+* Read []()
---
+++ functions.js
@@ -0,0 +1,189 @@
+/*
+ * PROBLEM 1:
+ * Write a function that takes a string and checks to make sure that the string
+ * has a length of three. Return true if the string length is three or false if
+ * it is not three.
+ *
+ * If the input is invalid throw an 'Invalid Input' exception.
+ */
+function checkData(inputString) {
+	// your code goes here
+}
+
+/*
+ * PROBLEM 2:
+ * Write a function that returns the sum of squares of all integers from 1 up to
+ * and including a given N.
+ *
+ * If the input is invalid throw an 'Invalid Input' exception.
+ */
+function sumSquares(N) {
+	// your code goes here
+}
+
+/*
+ * PROBLEM 3:
+ * Write a function that, given two arrays of integers a and b obtain the array
+ * formed by the elements of a followed by the elements of b.
+ *
+ * If the input is invalid throw an 'Invalid Input' exception.
+ */
+function concatenateArrays(a, b) {
+	// your code goes here
+}
+
+/*
+ * PROBLEM 4:
+ * Write a function that takes two arrays of integers a and b returns an array
+ * that includes only the elements that appear in both a and b.
+ *
+ * If the input is invalid throw an 'Invalid Input' exception.
+ */
+function findBoth(a, b) {
+	// your code goes here
+}
+
+/*
+ * PROBLEM 5:
+ * Write a function that takes two arrays of integers a and b. The function
+ * should return the number of elements that exist in both a and b.
+ *
+ * If the input is invalid throw an 'Invalid Input' exception.
+ */
+function countBoth(a, b) {
+	// your code goes here
+}
+
+/*
+ * PROBLEM 6:
+ * Proper nouns always begin with a capital letter, followed by small letters.
+ * Write a function called `fixProperNoun` that takes a potentially improperly
+ * capitalized proper noun and returns the same noun with the proper
+ * capitalization.
+ *
+ * If the input is invalid throw an 'Invalid Input' exception.
+ */
+
+/*
+ * PROBLEM 7:
+ * Write a function called `sortLetters` that returns a string that sorts all 
+ * the letters from a given string in the alphabetical order.
+ *
+ * If the input is invalid throw an 'Invalid Input' exception.
+ */
+
+/*
+ * PROBLEM 8:
+ * Write a function called `absVal` that return the absolute value of a given 
+ * integer.
+ *
+ * If the input is invalid throw an 'Invalid Input' exception.
+ */
+
+/* 
+ * PROBLEM 9:
+ * Given an array of integers, write a function called `findMaxDiff` that finds
+ * the maximal difference between any two adjacent elements.
+ *
+ * If the input is invalid throw an 'Invalid Input' exception.
+ */
+
+/*
+ * PROBLEM 10:
+ * Write a function called `insertDashes` that transforms a given sentence into
+ * a new one with dashes between each two consecutive letters.
+ * For example: insertDashes('abba test') => a-b-b-a t-e-s-t
+ *
+ * If the input is invalid throw an 'Invalid Input' exception.
+ */
+
+/* 
+ * PROBLEM 11:
+ * In linear algebra, a square matrix (array of arrays) is called a diagonal 
+ * matrix if all entries outside the main diagonal are zero (the diagonal from 
+ * the upper left to the lower right).
+ * For example:
+ * [
+ *	 [1, 0, 0],
+ *   [0, 2, 0],
+ *   [0, 0, 3]
+ * ]
+ * is a diagonal matrix.
+ *
+ * [
+ *	 [1, 0, 2],
+ *   [0, 2, 0],
+ *   [0, 0, 3]
+ * ]
+ * is not a diagonal matrix.
+ *
+ * [
+ *	 [1, 0, 0],
+ *   [0, 2, 0]
+ * ]
+ * is not a diagonal matrix.
+ *
+ * Write a function called `isDiagonalMatrix` that takes a matrix and returns
+ * true if the matrix is a diagonal matrix. Otherwise return false.
+ *
+ * If the input is invalid throw an 'Invalid Input' exception.
+ */
+
+/* 
+ * PROBLEM 12:
+ * Implement a function called `mySubstring` that can output the substring of 
+ * the given string within specified bounds.
+ * 
+ * For example: mySubstring('abcde', 2, 3) === 'cd'
+ * 
+ * Don't use String.substring in your solution.
+ *
+ * If the input is invalid throw an 'Invalid Input' exception.
+ */
+
+/*
+ * PROBLEM 13:
+ * Write a function called `splitSwap` that swaps two halves of a given array.
+ * If the array has an odd number of elements the array should be split in half
+ * by rounding down the number of elements divided by two.
+ * 
+ * For example: splitSwap([1,2,3,4,5]) === [3,4,5,1,2]
+ *
+ * If the input is invalid throw an 'Invalid Input' exception.
+ */
+
+/*
+ * PROBLEM 14:
+ * Write a function called `myMin` that takes two integers and returns the
+ * smaller of the two numbers.
+ *
+ * If the input is invalid throw an 'Invalid Input' exception.
+ */
+
+/*
+ * PROBLEM 15:
+ * Write a function called `myMin` that takes two integers and returns the
+ * smaller of the two numbers.
+ *
+ * If the input is invalid throw an 'Invalid Input' exception.
+ */
+
+/*
+ * PROBLEM 16:
+ * Write a function called `getMonth` that maps a given integer to a month.
+ *
+ * For example:
+ * getMonth(1) == 'January'
+ * getMonth(3) == 'March'
+ * etc.
+ *
+ * If the input is invalid throw an 'Invalid Input' exception.
+ */
+
+/*
+ * PROBLEM 16:
+ * For given n and k write a function called `smallMultiples` that returns the
+ * count the number of multiples of k that are not greater than n.
+ *
+ * If the input is invalid throw an 'Invalid Input' exception.
+ */

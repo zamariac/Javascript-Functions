306306e533b005b3d768a24927a5e86221d4a0ed
Aaron Larner <anlarner@gmail.com>
2015-04-15 15:09:02 -0500

    Update problems, ordering them by difficulty

--- functions.js
+++ functions.js
@@ -1,5 +1,5 @@
 /*
- * PROBLEM 1:
+ * PROBLEM 1: (easy)
  * Write a function that takes a string and checks to make sure that the string
  * has a length of three. Return true if the string length is three or false if
  * it is not three.
@@ -11,18 +11,7 @@ function checkData(inputString) {
 }
 
 /*
- * PROBLEM 2:
- * Write a function that returns the sum of squares of all integers from 1 up to
- * and including a given N.
- *
- * If the input is invalid throw an 'Invalid Input' exception.
- */
-function sumSquares(N) {
-	// your code goes here
-}
-
-/*
- * PROBLEM 3:
+ * PROBLEM 2: (easy)
  * Write a function that, given two arrays of integers a and b obtain the array
  * formed by the elements of a followed by the elements of b.
  *
@@ -33,29 +22,7 @@ function concatenateArrays(a, b) {
 }
 
 /*
- * PROBLEM 4:
- * Write a function that takes two arrays of integers a and b returns an array
- * that includes only the elements that appear in both a and b.
- *
- * If the input is invalid throw an 'Invalid Input' exception.
- */
-function findBoth(a, b) {
-	// your code goes here
-}
-
-/*
- * PROBLEM 5:
- * Write a function that takes two arrays of integers a and b. The function
- * should return the number of elements that exist in both a and b.
- *
- * If the input is invalid throw an 'Invalid Input' exception.
- */
-function countBoth(a, b) {
-	// your code goes here
-}
-
-/*
- * PROBLEM 6:
+ * PROBLEM 3: (easy)
  * Proper nouns always begin with a capital letter, followed by small letters.
  * Write a function called `fixProperNoun` that takes a potentially improperly
  * capitalized proper noun and returns the same noun with the proper
@@ -63,25 +30,63 @@ function countBoth(a, b) {
  *
  * If the input is invalid throw an 'Invalid Input' exception.
  */
+function fixProperNoun(noun) {
+	// your code goes here
+}
 
 /*
- * PROBLEM 7:
+ * PROBLEM 4: (easy)
  * Write a function called `sortLetters` that returns a string that sorts all 
  * the letters from a given string in the alphabetical order.
  *
  * If the input is invalid throw an 'Invalid Input' exception.
  */
+function sortLetters(noun) {
+	// your code goes here
+}
 
 /*
- * PROBLEM 8:
+ * PROBLEM 5: (easy)
  * Write a function called `absVal` that return the absolute value of a given 
  * integer.
  *
  * If the input is invalid throw an 'Invalid Input' exception.
  */
 
+/*
+ * PROBLEM 6: (easy)
+ * Write a function called `myMin` that takes two integers and returns the
+ * smaller of the two numbers.
+ *
+ * If the input is invalid throw an 'Invalid Input' exception.
+ */
+
+/*
+ * PROBLEM 7: (medium)
+ * Write a function called `findBoth` that takes two arrays of integers a and b returns an array
+ * that includes only the elements that appear in both a and b.
+ *
+ * If the input is invalid throw an 'Invalid Input' exception.
+ */
+
+/*
+ * PROBLEM 8: (medium)
+ * Write a function called `countBoth` that takes two arrays of integers a and b. The function
+ * should return the number of elements that exist in both a and b.
+ *
+ * If the input is invalid throw an 'Invalid Input' exception.
+ */
+
+/*
+ * PROBLEM 9: (medium)
+ * Write a function called `sumSquares` that returns the sum of squares of all integers from 1 up to
+ * and including a given N.
+ *
+ * If the input is invalid throw an 'Invalid Input' exception.
+ */
+
 /* 
- * PROBLEM 9:
+ * PROBLEM 10: (medium)
  * Given an array of integers, write a function called `findMaxDiff` that finds
  * the maximal difference between any two adjacent elements.
  *
@@ -89,7 +94,7 @@ function countBoth(a, b) {
  */
 
 /*
- * PROBLEM 10:
+ * PROBLEM 11: (medium)
  * Write a function called `insertDashes` that transforms a given sentence into
  * a new one with dashes between each two consecutive letters.
  * For example: insertDashes('abba test') => a-b-b-a t-e-s-t
@@ -98,7 +103,50 @@ function countBoth(a, b) {
  */
 
 /* 
- * PROBLEM 11:
+ * PROBLEM 12: (medium)
+ * Implement a function called `mySubstring` that can output the substring of 
+ * the given string within specified bounds.
+ * 
+ * For example: mySubstring('abcde', 2, 3) === 'cd'
+ * 
+ * Don't use String.substring in your solution.
+ *
+ * If the input is invalid throw an 'Invalid Input' exception.
+ */
+
+/*
+ * PROBLEM 13: (medium)
+ * Write a function called `splitSwap` that swaps two halves of a given array.
+ * If the array has an odd number of elements the array should be split in half
+ * by rounding down the number of elements divided by two.
+ * 
+ * For example: splitSwap([1,2,3,4,5]) === [3,4,5,1,2]
+ *
+ * If the input is invalid throw an 'Invalid Input' exception.
+ */
+
+/*
+ * PROBLEM 14: (medium)
+ * Write a function called `getMonth` that maps a given integer to a month.
+ *
+ * For example:
+ * getMonth(1) == 'January'
+ * getMonth(3) == 'March'
+ * etc.
+ *
+ * If the input is invalid throw an 'Invalid Input' exception.
+ */
+
+/*
+ * PROBLEM 15: (medium)
+ * For given n and k write a function called `smallMultiples` that returns the
+ * count the number of multiples of k that are not greater than n.
+ *
+ * If the input is invalid throw an 'Invalid Input' exception.
+ */
+
+/* 
+ * PROBLEM 16: (hard)
  * In linear algebra, a square matrix (array of arrays) is called a diagonal 
  * matrix if all entries outside the main diagonal are zero (the diagonal from 
  * the upper left to the lower right).
@@ -125,65 +173,6 @@ function countBoth(a, b) {
  *
  * Write a function called `isDiagonalMatrix` that takes a matrix and returns
  * true if the matrix is a diagonal matrix. Otherwise return false.
- *
- * If the input is invalid throw an 'Invalid Input' exception.
- */
-
-/* 
- * PROBLEM 12:
- * Implement a function called `mySubstring` that can output the substring of 
- * the given string within specified bounds.
- * 
- * For example: mySubstring('abcde', 2, 3) === 'cd'
- * 
- * Don't use String.substring in your solution.
- *
- * If the input is invalid throw an 'Invalid Input' exception.
- */
-
-/*
- * PROBLEM 13:
- * Write a function called `splitSwap` that swaps two halves of a given array.
- * If the array has an odd number of elements the array should be split in half
- * by rounding down the number of elements divided by two.
- * 
- * For example: splitSwap([1,2,3,4,5]) === [3,4,5,1,2]
- *
- * If the input is invalid throw an 'Invalid Input' exception.
- */
-
-/*
- * PROBLEM 14:
- * Write a function called `myMin` that takes two integers and returns the
- * smaller of the two numbers.
- *
- * If the input is invalid throw an 'Invalid Input' exception.
- */
-
-/*
- * PROBLEM 15:
- * Write a function called `myMin` that takes two integers and returns the
- * smaller of the two numbers.
- *
- * If the input is invalid throw an 'Invalid Input' exception.
- */
-
-/*
- * PROBLEM 16:
- * Write a function called `getMonth` that maps a given integer to a month.
- *
- * For example:
- * getMonth(1) == 'January'
- * getMonth(3) == 'March'
- * etc.
- *
- * If the input is invalid throw an 'Invalid Input' exception.
- */
-
-/*
- * PROBLEM 16:
- * For given n and k write a function called `smallMultiples` that returns the
- * count the number of multiples of k that are not greater than n.
  *
  * If the input is invalid throw an 'Invalid Input' exception.
  */
